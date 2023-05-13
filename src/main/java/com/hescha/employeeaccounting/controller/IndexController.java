25b00279067277acc4409c4bf6de1f487558294f
Yauhescha <evgescha.maslov007@yandex.ru>
2023-05-13 21:32:51 +0300

    prepare mock design

--- src/main/java/com/hescha/employeeaccounting/controller/IndexController.java
+++ src/main/java/com/hescha/employeeaccounting/controller/IndexController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
-    @GetMapping("/about")
-    public String about() {
-        return "about";
+    @GetMapping("/404")
+    public String _404() {
+        return "404";
     }
 
     @GetMapping("/contact")
@@ -21,32 +21,6 @@ public class IndexController {
     public String index() {
         return "index";
     }
-
-    @GetMapping("/single")
-    public String single() {
-        return "single";
-    }
-
-    @GetMapping("/404")
-    public String _404() {
-        return "404";
-    }
-
-    @GetMapping("/blog")
-    public String blog() {
-        return "blog";
-    }
-
-    @GetMapping("/gallery")
-    public String gallery() {
-        return "gallery";
-    }
-
-    @GetMapping("/icons")
-    public String icons() {
-        return "icons";
-    }
-
     @GetMapping("/team")
     public String team() {
         return "team";
